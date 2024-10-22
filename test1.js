ba6592a525309517870e7858cd52d819cbd62f2a
Syeda_munaza <imunazaizhar@gmail.com>
2024-10-22 16:34:11 +0500

    test2.js

---
+++ test1.js
@@ -0,0 +1,50 @@
+// Practice of var, let , const
+
+const myNum= 345;
+let myAge= 22;
+console.log(myAge);
+console.log(myNum);
+
+// Example with x y z
+x=23;
+y=7;
+z=x+y;
+console.log(x+y);
+
+// Examples with const 
+const price1=50;
+const price2=50;
+const price3=50;
+let total=price1+price2+price3;
+console.log(price1+price2+price3);
+
+//examples with firstName,etc
+let firstName="Munaza";
+let lastName="saba";
+let Age=45;
+console.table([firstName,lastName,Age]); 
+
+// Datatypes example There are 8 datatypes in Js like((string, number, boolean, object, symbol, null, undefine, bignit "123456789012345678901234567890"))
+let myName="Syeda Munaza" 
+let priceOfAc= 87900
+let isLoggedIn= false
+// null is a standalone value (interview question is that null is a object)
+// in undefine value we don't call any value like let myCollection;
+//symbol are use in figma tools for their uniqueness
+// objects
+
+console.log(typeof "munaza")
+console.log(typeof 67);
+console.log(typeof null);
+
+let a,b,c;
+a=44;
+b=44;
+c=a+b;
+
+
+
+
+
+
+
---
+++ test2.js
@@ -0,0 +1,38 @@
+// Conersion and operation in js
+
+
+
+
+let score="34abc"
+console.log(typeof score);
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let myAge=null
+console.log(typeof myAge);
+
+
+console.log(2+2);
+console.log(null>=0);
+console.log('value: ', 2+"4"+2);
+console.log(56>0);
+
+// I don't understand these type of rubbish coding
+
+// Data types are divided into 2 parts primitive and non-primitive
+// primitive are of 7 types: string(call by value and it is given to you as a copied form), Number, Boolean, null, undefined, Symbol, BigInt
+// Reference(Non-primitive data types) are Array, Objects, Fuunction
+
+const id=Symbol('123')
+const anotherId= Symbol('123')
+console.log(id===anotherId);
+
+let myYoutube= "munshaizhar"
+let anothername="saboo"
+console.log(anothername);
+
+
+
+  
